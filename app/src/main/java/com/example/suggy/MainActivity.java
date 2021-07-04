10ad5a27e5bcf9c407e3547a89dc87d031aa102b
PandaPandula <alexandre.flo@estudiantat.upc.edu>
2021-07-04 12:36:08 +0200

    asdfdasf

--- app/src/main/java/com/example/suggy/MainActivity.java
+++ app/src/main/java/com/example/suggy/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button hola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
