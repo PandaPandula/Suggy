50fa8e53417c7771e2c622c2cf0345b900d3f26c
Roger Masc <rogersteam123@gmail.com>
2021-07-04 12:41:15 +0200

    Update3

--- app/src/main/java/com/example/suggy/MainActivity.java
+++ app/src/main/java/com/example/suggy/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button hola, hola2;
+    private Button hola, hola2,hola3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
