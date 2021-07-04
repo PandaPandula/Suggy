fa60fb3222af64b761ce4b44d66edd2aff8d7c38
PandaPandula <alexandre.flo@estudiantat.upc.edu>
2021-07-04 11:33:47 +0200

    Second Commit

--- app/src/main/java/com/example/suggy/MainActivity.java
+++ app/src/main/java/com/example/suggy/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.suggy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    private Button hola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
